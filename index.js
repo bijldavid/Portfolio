cb96f43454814f81e782d0084a60ea3a2876721a
[bijldavid] <[davidbijl321@gmail.com]>
2024-02-11 02:30:32 +0100

    infinite scrolling bar is working

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ function toggleMenu() {
   deHeader.classList.toggle("open");
   body.classList.toggle("scroll-lock"); // Add or remove the scroll-lock class
 }
+
+const scrollers = document.querySelectorAll(".main2 section:nth-of-type(1)");
+
+// Call the addAnimation function to start the animation
+addAnimation();
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+
+    const scrollerInner = scroller.querySelector(".main2 section:nth-of-type(1) ul");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
